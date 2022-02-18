62fe94dbcd55ecc85a63ca600d4d58715c0f2191
Mohammad Mousad <moh.moussad@gmail.com>
2022-02-18 19:30:08 +0200

    add sorting functions using moment.js

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -5,32 +5,36 @@ const user = {
   age: 25,
 };
 
+// Filters Decleration
 const filters = {
   searchText: "",
+  sortBy: "byEdited",
 };
+document.querySelector("#search-text").addEventListener("input", function (e) {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
+document.querySelector("#filter-by").addEventListener("change", function (e) {
+  filters.sortBy = e.target.value;
+  renderNotes(notes, filters);
+});
 
 renderNotes(notes, filters);
 
 document.querySelector("#create-note").addEventListener("click", function (e) {
   const id = uuidv4();
+  const timestamp = moment().valueOf();
   notes.push({
     id: id,
     title: "",
     body: "",
+    createdAt: timestamp,
+    updatedAt: timestamp,
   });
+  console.log(notes);
 
   saveNotes(notes);
-  // renderNotes(notes, filters);
   location.assign(`/notes-app/edit.html#${id}`);
-});
-
-document.querySelector("#search-text").addEventListener("input", function (e) {
-  filters.searchText = e.target.value;
-  renderNotes(notes, filters);
-});
-
-document.querySelector("#filter-by").addEventListener("change", function (e) {
-  console.log(e.target.value);
 });
 
 window.addEventListener("storage", function (e) {
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -19,6 +19,8 @@ const titleElement = document.querySelector("#note-title");
 titleElement.value = note.title;
 titleElement.addEventListener("input", function (e) {
   note.title = e.target.value;
+  note.updatedAt = moment().valueOf();
+  dateElement.textContent = generateLastEdited(note.updatedAt);
   saveNotes(notes);
 });
 
@@ -27,6 +29,9 @@ const bodyElement = document.querySelector("#note-body");
 bodyElement.value = note.body;
 bodyElement.addEventListener("input", function (e) {
   note.body = e.target.value;
+  note.updatedAt = moment().valueOf();
+  dateElement.textContent = generateLastEdited(note.updatedAt);
+
   saveNotes(notes);
 });
 
@@ -50,5 +55,10 @@ window.addEventListener("storage", function (e) {
     }
     titleElement.value = note.title;
     bodyElement.value = note.body;
+    dateElement.textContent = generateLastEdited(note.updatedAt);
   }
 });
+
+// Last edited element
+const dateElement = document.querySelector("#last-edited");
+dateElement.textContent = generateLastEdited(note.updatedAt);
